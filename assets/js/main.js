0fb473eec9785aad19a205b61b1a8f6a49e7379a
drtrancelecter <acoustic.skylinechile@gmail.com>
2025-09-10 21:10:54 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,1 @@
-// Archivo base — reservado por si añadimos router hash más adelante.
-// Por ahora, nada funcional para que no rompa el layout.
-console.log('INSEMAD site ok');
+// reservado para funciones futuras
